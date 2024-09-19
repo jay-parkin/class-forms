1ec6f9e2dc4ac5d31c068317a5edd2b14d91c2c3
Jason Parkin <jay-parkin91@hotmail.com>
2024-09-19 19:43:42 +1000

    feat: component structures started

---
+++ src/components/SocialMediaPostContainer..jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReadonlyPost from "./ReadOnlyPost";
+
+export default class SocialMediaPostContainer extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            isEditing: false,
+            author: "",
+            dateCreated: Date.now(),
+            lastUpdated: Date.now(),
+            location: "",
+            content: ""
+        }
+    }
+
+    render() {
+        if (this.state.isEditing) {
+            return (
+                <EditablePost
+                    author={this.state.author}
+                    dateCreated={this.state.dateCreated}
+                    lastUpdated={this.state.lastUpdated}
+                    location={this.state.location}
+                    content={this.state.content}
+
+                    parentState={this.state}
+                />
+            );
+        } else {
+            return (
+                <ReadonlyPost
+                    author={this.state.author}
+                    dateCreated={this.state.dateCreated}
+                    lastUpdated={this.state.lastUpdated}
+                    location={this.state.location}
+                    content={this.state.content}
+
+                    parentState={this.state}
+                />
+            );
+        }
+    }
+}
